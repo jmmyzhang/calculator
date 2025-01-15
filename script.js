257486391c2ca54f07c4af621fddc6ba0f7a13f1
jmmyzhang <155680424+jmmyzhang@users.noreply.github.com>
2025-01-15 13:44:27 -0500

    add operate function

---
+++ script.js
@@ -0,0 +1,23 @@
+let number1;
+let number2;
+let operator;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num1, num2, op) {
+    return op(num1, num2);
+}
