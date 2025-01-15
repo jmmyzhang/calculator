3225c2f3b97385074b9f39ac0d328dc215cf426e
jmmyzhang <155680424+jmmyzhang@users.noreply.github.com>
2025-01-15 14:13:21 -0500

    add display update

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ let number1;
 let number2;
 let operator;
 
+let display = document.querySelector("#display");
+let frame = document.querySelector(".frame");
+
+frame.addEventListener("click", (e) => {
+    let target = e.target;
+    if (target.classList == "num") {
+        updateDisplay(target.id);
+    }
+})
+
+
 function add(a, b) {
     return a + b;
 }
@@ -20,4 +31,8 @@ function divide(a, b) {
 
 function operate(num1, num2, op) {
     return op(num1, num2);
+}
+
+function updateDisplay(input) {
+    display.innerText += input;
 }
